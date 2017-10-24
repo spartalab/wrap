cce6fc82e0f60b69c77738f68624e7b2a70c92da
William E. Alexander <williamericalexander@gmail.com>
2017-10-24 15:49:29 -0500

    Initial commit - Hello, World
    Creating a Hello, World application to establish the repository

---
+++ src/edu/utexas/algorithmB/algorithmB.java
@@ -0,0 +1,8 @@
+package edu.utexas.algorithmB;
+
+public class algorithmB{
+	public static void main(String[] args) {
+		// The very first line of code!
+		System.out.println("Hello, World!");
+	}
+}
