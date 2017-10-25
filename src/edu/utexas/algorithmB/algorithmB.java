5c7d4f82e89f84ffeee026461b82e764ed833b45
William E. Alexander <williamericalexander@gmail.com>
2017-10-24 20:17:51 -0500

    Introduction documentation, and some ASCII art
    Created the first introduction documentation paragraphs and added ASCII
    drawing of The Tower. Provided initial citations for Algorithm B and the
    Traffic Assignment Problem.

--- src/edu/utexas/algorithmB/algorithmB.java
+++ src/edu/utexas/algorithmB/algorithmB.java
@@ -1,8 +1,83 @@
 package edu.utexas.algorithmB;
 
+/** Algorithm B implementation
+ * @author William E. Alexander
+ * @author Adam Nodjomian
+ * @author Rahul Patel
+ * @author Prashanth Venkatraman
+ * 
+ * This project will implement Algorithm B, as presented 
+ * by Dial (2006) in order to create a solver for the
+ * Traffic Assignment Problem as defined by Beckmann et 
+ * al. (1956). The aim for this project is to implement a
+ * time- and space-efficient framework of Algorithm B
+ * which can be studied, extended, and executed properly.
+ * We also aim to provide quality documentation and testing
+ * of our code, in order that this project will serve as a
+ * good exercise in practicing proper software development
+ * techniques. 
+ * 
+ * This project will be completed as part of the requirements
+ * for the CE 392C Transportation Network Analysis course 
+ * at the University of Texas at Austin during the Fall 2017
+ * semester, with the potential for further development to
+ * follow. 
+ * 
+ *            _______
+ *          _|_______|_
+ *         [ [_  _  _] ]
+ *         !|| || || ||!
+ *         !|| || || ||!
+ *         !|| || || ||!
+ *         [[]_[]_[]_[]]
+ *         -------------
+ *         |  |  .  |  |
+ *         |  |..+..|  |
+ *      _  |  / _?_ \  |  _
+ *     [ ]_|_[]/   \[]_|_[ ]
+ *     |     |(__/  )|     |
+ *     |_____|_\___/_|_____|
+ *    /| ..  |   ~   |  .. |\
+ *   [___][ " ][ " ][ " ][___]
+ *   |_______________________|
+ *   |     |#|  |#|  |#|     |
+ *   |     |#|  |#|  |#|     |
+ *   |     | |  | |  | |     |
+ *   |     |#|  |#|  |#|     |
+ *   |     | |  | |  | |     |
+ *   |     |#|  |#|  |#|     |
+ *   |     | |  | |  | |     |
+ *   |     |#|  |#|  |#|     |
+ *   |     | |  | |  | |     |
+ *   |     |#|  |#|  |#|     |
+ *   |     | |  | |  | |     |
+ *   |     |#|  |#|  |#|     |
+ *   |     | |  | |  | |     |
+ *   |     |#|  |#|  |#|     |
+ *   |     | |  | |  | |     |
+ *   |     |#|  |#|  |#|     |
+ *   |     | |  | |  | |     |
+ *   |     |#|  |#|  |#|     |
+ *   |     | |  | |  | |     |
+ *   |     |#|  |#|  |#|     |
+ *   |     | |  | |  | |     |
+ *   |     |#|  |#|  |#|     |
+ *   |     | |  | |  | |     |
+ *   |     |#|  |#|  |#|     |
+ *   |     | |  | |  | |     |
+ *   |     |#|  |#|  |#|     |
+ *   |     | |  | |  | |     |
+ *   |     |#|  |#|  |#|     |
+ *   |     | |  | |  | |     |
+ *   |_____|#|__|#|__|#|_____|
+ *   [_______________________]  
+ *   |    HOOK 'EM HORNS!    |
+ *   
+ */
 public class algorithmB{
 	public static void main(String[] args) {
 		// The very first line of code!
 		System.out.println("Hello, World!");
 	}
 }
+
