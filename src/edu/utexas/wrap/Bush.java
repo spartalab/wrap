c068ca85ac9b871f44b065f6ac53c7349739745b
William E. Alexander <williamericalexander@gmail.com>
2017-11-04 15:20:28 -0500

    Implementing Dijkstra's algorithm for initial Bush creation (incomplete)
    Implemented Dijkstra's in Origin's buildBush method. Creates initial
    nodeL and qShort mapping. This doesn't yet store the links and nodes in
    the bush object, cuz I think we need to talk about the Bush class.

--- src/edu/utexas/wrap/Bush.java
+++ src/edu/utexas/wrap/Bush.java
@@ -1,13 +1,21 @@
 package edu.utexas.wrap;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Bush extends Network{
 	private Origin origin;
 	
-	public Bush(Map<Integer,Node> nodes, List<Link> links, List<Origin> origin) {
-		super(/*nodes,*/links,origin);
+	public Bush(List<Link> links, List<Origin> origin) {
+		super(links,origin);
 		this.origin = origin.get(0);
 	}
+	
+	public Bush(List<Link> links, Origin origin) {
+		super(links, origin);
+		this.origin = origin;
+	}
+	
+	
 }
