31371975e6acd7609b6c931ba435765a36e42620
William E. Alexander <williamericalexander@gmail.com>
2017-11-20 19:11:43 -0600

    Finding divergence nodes
    Implement method of finding divergence node for balancing path flows.
    Doesn't build a path yet (to be developed)

--- src/edu/utexas/wrap/AlgorithmBOptimizer.java
+++ src/edu/utexas/wrap/AlgorithmBOptimizer.java
@@ -1,6 +1,9 @@
 package edu.utexas.wrap;
 
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Set;
 
 public class AlgorithmBOptimizer extends Optimizer{
 
@@ -20,13 +23,59 @@ public class AlgorithmBOptimizer extends Optimizer{
 			
 			while (topOrder.hasNext()) {
 				Node cur = topOrder.next();
-
-				if (b.getqLong(cur).equals(b.getqShort(cur))) {
+				Link shortLink = b.getqShort(cur);
+				Link longLink = b.getqLong(cur);
+				if (longLink.equals(shortLink)) {
 					// If there is no divergence node
 					continue;
 				}
 				else {
 					//TODO calculate divergence node
+					LinkedList<Link> uPath = new LinkedList<Link>();
+					LinkedList<Link> lPath = new LinkedList<Link>();
+					
+					Integer lenLong = 2;
+					Integer lenShort = 2;
+					
+					Node longNode = longLink.getTail();
+					Node shortNode = shortLink.getTail();
+					Node divergence;
+					while (!longNode.equals(o)) {
+						lenLong++;
+						longNode = b.getqLong(longNode).getTail();
+					}
+					while (!shortNode.equals(o)) {
+						lenShort++;
+						shortNode = b.getqShort(shortNode).getTail();
+					}
+					
+					Node u = cur, l = cur;
+					if (lenLong > lenShort) {
+						
+						for (int i = 0; i < lenLong - lenShort; i++) {
+							u = b.getqLong(u).getTail();
+						}
+					} else {
+						
+						for (int i = 0; i < lenShort - lenLong; i++) {
+							l = b.getqShort(l).getTail();
+						}
+					}
+					
+					shortLink = b.getqShort(l);
+					longLink = b.getqLong(u);
+					
+					while (longLink != null && shortLink != null) {
+						u = longLink.getTail();
+						l = shortLink.getTail();
+						if (!u.equals(l)){
+							longLink = b.getqLong(u);
+							shortLink = b.getqShort(u);
+						}
+						else break;
+					}
+					divergence = l;
+					
 				}
 				//TODO build pair of alternate segments pi_L and pi_U
 				
