de778c25b9e26fe2733a0764904d61079bbb44ed
wea326 <wea326@CAEE-BOYLESLB06.austin.utexas.edu>
2018-07-05 19:33:50 -0500

    cleanup (waxing floors so we gotta boogie)

---
+++ src/edu/utexas/wrap/NegativeFlowException.java
@@ -0,0 +1,13 @@
+package edu.utexas.wrap;
+
+public class NegativeFlowException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3665578229066726356L;
+
+	public NegativeFlowException(String e) {
+		super(e);
+	}
+}
