6d28b9d838a0b218bff3303d984714207b3a1495
Rishabh Thakkar <rishabh.thakkar@gmail.com>
2018-11-10 16:21:56 -0600

    Finished Cassandra Implementation
    Will not work with Java 10, however.

--- src/edu/utexas/wrap/Link.java
+++ src/edu/utexas/wrap/Link.java
@@ -1,19 +1,17 @@
 package edu.utexas.wrap;
 
-import org.postgresql.core.SqlCommand;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.HashMap;
-import java.util.Map;
-import java.sql.*;
-//import java.sql.DriverManager;
+
+import com.datastax.driver.core.*;
+import com.datastax.driver.core.Cluster.Builder;
+
 /**
  * @author rahulpatel
  *
  */
 public abstract class Link implements Priced {
-
 	private static final String dbName = "network";
 
 	private final Float capacity;
@@ -25,26 +23,18 @@ public abstract class Link implements Priced {
 	private BigDecimal cachedFlow = null;
 	protected BigDecimal cachedTT = null;
 	protected BigDecimal cachedPrice = null;
-	private static Connection databaseCon;
-//HEY!
-	 private final String createQuery = "CREATE TABLE t" + hashCode() + " (" +
+	private static Session databaseCon;
+ 	private final String createQuery = "CREATE TABLE t" + hashCode() + " (" +
 			"bush_origin_id integer, " +
 			"vot real, " +
 			"vehicle_class text, " +
 			"flow decimal(" + Optimizer.defMC.getPrecision() + ")," +
-			"UNIQUE (bush_origin_id, vot, vehicle_class))";
+			"PRIMARY KEY (bush_origin_id, vot, vehicle_class))";
 	private final String sumQuery = "SELECT SUM (flow) AS totalFlow FROM t" + hashCode();
 
 	private final String updateQuery = "INSERT INTO t" + hashCode() + " (bush_origin_id, vot, vehicle_class, flow) " +
 			"VALUES (" +
-			"?,?,?,?)" +
-			"ON CONFLICT (bush_origin_id, vot, vehicle_class)" +
-			"DO UPDATE " +
-			"SET flow = ? " +
-			"WHERE " +
-			"t" +hashCode()+".bush_origin_id = ? " +
-			"AND t"+hashCode()+".vot = ? " +
-			"AND t"+hashCode()+".vehicle_class = ?";
+			"?,?,?,?)";
 
 	private final String deleteQuery = "DELETE FROM t" + hashCode() +
 			" WHERE " +
@@ -52,26 +42,22 @@ public abstract class Link implements Priced {
 			"AND vot = ? " +
 			"AND vehicle_class = ?";
 
-	private final String selectQuery = "SELECT * FROM t" + hashCode() +
+	private final String selectQuery = "SELECT 1 FROM t" + hashCode() +
 			" WHERE " +
 			"bush_origin_id = ? " +
 			"AND vot = ? " +
-			"AND vehicle_class = ?" +
-			"LIMIT 1";
+			"AND vehicle_class = ?";
 
 	private final String dropQuery = "DROP TABLE t" + hashCode();
 	static {
-		try {
-			Class.forName("org.postgresql.Driver");
-			databaseCon = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbName);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println("Could not find database/table to connect to");
-			System.exit(3);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+        Builder b = Cluster.builder().addContactPoint("127.0.0.1");
+        b.withPort(9142);
+        Cluster cluster = b.build();
+
+        Session session = cluster.connect();
+        session.execute("Create KEYSPACE "+ dbName +" IF NOT EXISTS WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 1}");
+        session.execute("Use " + dbName);
+		databaseCon = session;
 	}
 
 	public Link(Node tail, Node head, Float capacity, Float length, Float fftime) {
@@ -85,33 +71,20 @@ public abstract class Link implements Priced {
 
 
 	private void createTable() {
-		Statement stm;
-		try {
-		    stm = databaseCon.createStatement();
-		    stm.execute(createQuery);
-        } catch (SQLException e) {
-			e.printStackTrace();
-			System.exit(1);
-        }
+		SimpleStatement stm = new SimpleStatement(createQuery);
+		databaseCon.execute(stm);
 	}
 
 	private BigDecimal totalFlowFromTable() {
-		Statement stm;
-		try {
-			stm = databaseCon.createStatement();
-			BigDecimal total = null;
-			ResultSet result = stm.executeQuery(sumQuery);
-			if(result.next()) {
-				total = result.getBigDecimal("totalFlow");
-			}
-			if (total == null)
-				return BigDecimal.ZERO;
-			return total;
-		} catch (SQLException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-		return BigDecimal.ZERO;
+        BigDecimal total = null;
+        ResultSet result = databaseCon.execute(sumQuery);
+        Row out = result.one();
+        if(out != null) {
+            total = out.getDecimal("totalFlow");
+        }
+        if (total == null)
+            return BigDecimal.ZERO;
+        return total;
 	}
 	public Float getCapacity() {
 		return capacity;
@@ -172,110 +145,62 @@ public abstract class Link implements Priced {
 		//Retrieve Bush with the specified parameters
 		PreparedStatement stm;
 		BigDecimal updateFlow = getBushFlow(bush);
-		try {
-			updateFlow = updateFlow.add(delta).setScale(Optimizer.decimalPlaces, RoundingMode.HALF_EVEN);
-			if(updateFlow.compareTo(BigDecimal.ZERO) < 0) throw new NegativeFlowException("invalid alter request");
-			else if(updateFlow.compareTo(BigDecimal.ZERO) > 0) {
-				stm = databaseCon.prepareStatement(updateQuery);
-				stm.setInt(1, bush.getOrigin().getID());
-				stm.setFloat(2, bush.getVOT());
-                if(bush.getVehicleClass() != null)
-                    stm.setString(3, bush.getVehicleClass().name());
-                else
-                    stm.setString(3, bush.toString());				stm.setBigDecimal(4, updateFlow);
-				stm.setBigDecimal(5, updateFlow);
-				stm.setInt(6, bush.getOrigin().getID());
-				stm.setFloat(7, bush.getVOT());
-				if(bush.getVehicleClass() != null)
-				    stm.setString(8, bush.getVehicleClass().name());
-				else
-				    stm.setString(8, bush.toString());
-				stm.executeUpdate();
-				return true;
-			} else {
-
-				stm = databaseCon.prepareStatement(deleteQuery);
-				stm.setInt(1, bush.getOrigin().getID());
-				stm.setFloat(2, bush.getVOT());
-				if(bush.getVehicleClass() != null)
-				    stm.setString(3, bush.getVehicleClass().name());
-				else
-				    stm.setString(3, bush.toString());
-				stm.executeUpdate();
-				return false;
-			}
-		} catch (SQLException e) {
-			//System.out.println("alter bush flow");
-			//System.out.println("SQL Error Code: " + e.getErrorCode());
-			e.printStackTrace();
-			System.exit(1);
-			return false;
-		}
-		//If Bush exists, get the flow and add delta to flow
-			//If new flow is < 0 throw error
-			//if new flow == 0 remove the row and return false
-			//If new flow is > 0 update the row in the table
-		//Otherwise add Bush with delta flow
-//		BigDecimal newFlow = flow.getOrDefault(bush,BigDecimal.ZERO).add(delta).setScale(Optimizer.decimalPlaces, RoundingMode.HALF_EVEN);
-//		if (newFlow.compareTo(BigDecimal.ZERO) < 0) throw new NegativeFlowException("invalid alter request");
-//		else if (newFlow.compareTo(BigDecimal.ZERO) > 0) flow.put(bush, newFlow);
-//		else {
-//			flow.remove(bush);
-//			return false;
-//		}
-//		return true;
+        updateFlow = updateFlow.add(delta).setScale(Optimizer.decimalPlaces, RoundingMode.HALF_EVEN);
+        if(updateFlow.compareTo(BigDecimal.ZERO) < 0) throw new NegativeFlowException("invalid alter request");
+        else if(updateFlow.compareTo(BigDecimal.ZERO) > 0) {
+            stm = databaseCon.prepare(updateQuery);
+            BoundStatement bound;
+            if(bush.getVehicleClass() != null)
+                bound = stm.bind(bush.getOrigin().getID(), bush.getVOT(), bush.getVehicleClass().name(), updateFlow);
+            else
+                bound = stm.bind(bush.getOrigin().getID(), bush.getVOT(), bush.toString(), updateFlow);
+            databaseCon.execute(bound);
+            return true;
+        } else {
+            stm = databaseCon.prepare(deleteQuery);
+            BoundStatement bound;
+            if(bush.getVehicleClass() != null)
+                bound = stm.bind(bush.getOrigin().getID(), bush.getVOT(), bush.getVehicleClass().name(), updateFlow);
+            else
+                bound = stm.bind(bush.getOrigin().getID(), bush.getVOT(), bush.toString(), updateFlow);
+            databaseCon.execute(bound);
+            return false;
+        }
 	}
 
 	public BigDecimal getBushFlow(Bush bush) {
 		PreparedStatement stm;
-		try {
-			stm = databaseCon.prepareStatement(selectQuery);
-			stm.setInt(1, bush.getOrigin().getID());
-			stm.setFloat(2, bush.getVOT());
-			if(bush.getVehicleClass() != null)
-			    stm.setString(3, bush.getVehicleClass().name());
-			else
-			    stm.setString(3, bush.toString());
-			ResultSet result = stm.executeQuery();
-			if(result.next()) {
-				return result.getBigDecimal("flow");
-			}
-		} catch (Exception e) {
-			//System.out.println("getBush flow");
-			//System.out.println("SQL Error Code: " + e.getErrorCode());
-			e.printStackTrace();
-			System.exit(1);
-			return BigDecimal.ZERO;
-		}
+        stm = databaseCon.prepare(selectQuery);
+        BoundStatement bound;
+        if(bush.getVehicleClass() != null)
+            bound = stm.bind(bush.getOrigin().getID(), bush.getVOT(), bush.getVehicleClass().name());
+        else
+            bound = stm.bind(bush.getOrigin().getID(), bush.getVOT(), bush.toString());
+        ResultSet out = databaseCon.execute(bound);
+        Row one = out.one();
+        if(one != null) {
+            return one.getDecimal("flow");
+        }
 		return BigDecimal.ZERO;
 	}
 
 	public Boolean hasFlow(Bush bush) {
-		PreparedStatement stm;
-		try {
-			stm = databaseCon.prepareStatement(selectQuery);
-			stm.setInt(1, bush.getOrigin().getID());
-			stm.setFloat(2, bush.getVOT());
-			if(bush.getVehicleClass() != null)
-			    stm.setString(3, bush.getVehicleClass().name());
-			else
-			    stm.setString(3, bush.toString());
-			return stm.executeQuery().next();
-		} catch (SQLException e) {
-			//System.out.println("has flow");
-			//System.out.println("SQL Error Code: " + e.getErrorCode());
-			e.printStackTrace();
-			System.exit(1);
-			return false;
-		}
-		//return flow.get(bush) != null;
+        PreparedStatement stm;
+        stm = databaseCon.prepare(selectQuery);
+        BoundStatement bound;
+        if(bush.getVehicleClass() != null)
+            bound = stm.bind(bush.getOrigin().getID(), bush.getVOT(), bush.getVehicleClass().name());
+        else
+            bound = stm.bind(bush.getOrigin().getID(), bush.getVOT(), bush.toString());
+        ResultSet out = databaseCon.execute(bound);
+        return !out.isExhausted();
 	}
 
 	public void removeTable() {
 		try{
-			Statement stm = databaseCon.createStatement();
-			stm.executeUpdate(dropQuery);
-		} catch (SQLException e) {
+			Statement stm = new SimpleStatement(dropQuery);
+			databaseCon.execute(dropQuery);
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
 			//System.out.println("removeTable");
