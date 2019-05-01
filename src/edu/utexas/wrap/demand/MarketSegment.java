f5bf71e60336adac46a244b62ea60122d710c3bb
Rishabh Thakkar <rishabh.thakkar@gmail.com>
2019-04-30 22:24:38 -0500

    Add Documentation and Trip Balancing. Also reflect column name changes

--- src/edu/utexas/wrap/demand/MarketSegment.java
+++ src/edu/utexas/wrap/demand/MarketSegment.java
@@ -12,6 +12,7 @@ import edu.utexas.wrap.net.Node;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Set;
 
@@ -38,11 +39,9 @@ public class MarketSegment {
     private Float vot;
     private String table_suffix;
     private int hash;
-    private String fftable;
-    private Class<? extends TripDistributor> distributorClass;
     private Set<String> modalMatricies;
 
-    public MarketSegment(Graph g, Connection db, boolean homeBased, boolean workTrip, char peakTrip, char incomeQ, boolean hasVeh, boolean asManyVehAsWork, float vot, Class<? extends TripDistributor> dist, String fftable) {
+    public MarketSegment(Graph g, Connection db, boolean homeBased, boolean workTrip, char peakTrip, char incomeQ, boolean hasVeh, boolean asManyVehAsWork, float vot) {
         if(peakTrip != 'a' && peakTrip != 'p' && peakTrip != 'o')
             throw new IllegalArgumentException("peakTrip must be 'a' or 'p' or 'o' ");
         if(incomeQ != '1' && incomeQ != '2' && incomeQ != '3' && incomeQ != '4')
@@ -59,16 +58,25 @@ public class MarketSegment {
         this.g = g;
         this.db = db;
         this.vot = vot;
-        this.fftable = fftable;
-        distributorClass = dist;
-
-
         hash = hashCode();
     }
 
+    /**
+     * This method generates the PA map using SQL linear algebra.
+     *
+     *  The generatePA SQL statment uses inner joins to compute a "dot-product" of rates and values to produce the PA Map.
+     *
+     *  The productions and attractions are calculated by an inner join between the attraction rates table, the production
+     *  rates table, and the demographic data table on the demographics of the model. It then multiples the rates to the
+     *  respective values in the demographic data table and sums them for each node. This produces the productions and
+     *  attractions values that are inserted along with the the node ID into the paMap table.
+     * @param demographicsTable table with the demographics values: (node, dem, value)
+     * @param productionRates table with the production rates associated with the market segment: (seg, dem, rate)
+     * @param attractionRates table with the attraction rates associated with the market segment: (seg, dem, rate)
+     */
     public void tripGeneration(String demographicsTable, String productionRates, String attractionRates) {
         String createQuery = "CREATE TABLE  ?" +
-                " (origin integer, " +
+                " (node integer, " +
                 "productions real, " +
                 "attractions real )";
         try(PreparedStatement ps = db.prepareStatement(createQuery)) {
@@ -79,10 +87,13 @@ public class MarketSegment {
             System.exit(1);
         }
 
-        String generatePA = "INSERT INTO ? (origin, productions, attractions)\n" +
-                "SELECT origin, sum(p.rate * dp.value), sum(a.rate * dp.value)\n" +
+        /*
+         *
+         */
+        String generatePA = "INSERT INTO ? (node, productions, attractions)\n" +
+                "SELECT node, sum(p.rate * dp.value), sum(a.rate * dp.value)\n" +
                 "  FROM ((SELECT * from ?) as dp INNER JOIN (SELECT * FROM ? WHERE seg=?) as p ON\n" +
-                "  dp.dem = p.dem INNER JOIN (SELECT * FROM ? WHERE seg=?) as a ON dp.dem=a.dem) group by origin;\n";
+                "  dp.dem = p.dem INNER JOIN (SELECT * FROM ? WHERE seg=?) as a ON dp.dem=a.dem) group by node;\n";
         try(PreparedStatement ps = db.prepareStatement(generatePA)) {
             ps.setString(1, paMapPrefix+table_suffix);
             ps.setString(2, demographicsTable);
@@ -97,15 +108,46 @@ public class MarketSegment {
         }
     }
 
-    public void tripDistribution() {
+    /**
+     * This function runs a basic implementation of trip balancing.
+     * It essentially finds the ratio between the productions
+     * and the attractions and multiplies each of the productions by this factor to match the the number of attractions
+     */
+    public void tripBalance() {
+        String sumQuery = "SELECT sum(productions) AS prodTotal, sum(attractions) AS attrTotal FROM ?";
+        try(PreparedStatement ps = db.prepareStatement(sumQuery)) {
+            ps.setString(1, getPAMapTable());
+            ResultSet output = ps.executeQuery();
+            float prodTotal = output.getFloat("prodTotal");
+            float attrTotal = output.getFloat("attrTotal");
+            float factor = attrTotal/prodTotal;
+            String factorQuery = "UPDATE ? SET productions = productions * ?";
+            PreparedStatement fq = db.prepareStatement(factorQuery);
+            fq.setString(1, getPAMapTable());
+            fq.setFloat(2, factor);
+            fq.executeUpdate();
+        } catch (SQLException s) {
+            s.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * This function performs Trip Distribution using the specified tripDistributor model and a friction factors table
+     * The tripDistributor model creates an in-memory PAHasMatrix which is then written back into a paMtx table in the
+     * database
+     * @param dist Trip Distribution Model being used
+     * @param fftable a friction factors table with peak roadway skims as the friction factor: (origin, destination, pktime)
+     */
+    public void tripDistribution(Class<? extends TripDistributor> dist, String fftable) {
         FrictionFactorMap ff = new FrictionFactorMap(this.g, this.db, fftable);
         try {
-            TripDistributor distributor = (TripDistributor) Class.forName(distributorClass.getName()).getConstructor(distributorClass).newInstance(new Object[]{g, ff});
+            TripDistributor distributor = (TripDistributor) Class.forName(dist.getName()).getConstructor(dist).newInstance(new Object[]{g, ff});
             AggregatePAHashMatrix aggPAMtx = (AggregatePAHashMatrix) distributor.distribute(new DBPAMap(g, paMapPrefix+table_suffix,db, vot));
 
             String createQuery = "CREATE TABLE  ?" +
-                    " (producer integer, " +
-                    "attractor integer, " +
+                    " (origin integer, " +
+                    "destination integer, " +
                     "demand real)";
             try(PreparedStatement ps = db.prepareStatement(createQuery)) {
                 ps.setString(1, paMtxPrefix+table_suffix);
