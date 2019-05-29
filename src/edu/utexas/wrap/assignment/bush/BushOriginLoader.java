93f9d57a0da80f3541b2c75bcc9639b15e4494bc
William Alexander <williamericalexander@gmail.com>
2019-05-29 14:41:24 -0500

    it would help if I staged the correct file...

--- src/edu/utexas/wrap/assignment/bush/BushOriginLoader.java
+++ src/edu/utexas/wrap/assignment/bush/BushOriginLoader.java
@@ -1,6 +1,9 @@
 package edu.utexas.wrap.assignment.bush;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Set;
 
 import edu.utexas.wrap.demand.AutoDemandMap;
@@ -9,7 +12,8 @@ import edu.utexas.wrap.net.Graph;
 import edu.utexas.wrap.net.Node;
 
 public class BushOriginLoader extends BushOriginBuilder {
-
+	Boolean print = true;
+	
 	public BushOriginLoader(Graph g, Node o, Set<BushOrigin> origins) {
 		super(g, o, origins);
 	}
@@ -22,8 +26,26 @@ public class BushOriginLoader extends BushOriginBuilder {
 				AutoDemandMap odm = map.get(c).get(vot);
 				if (!odm.isEmpty()) try {
 					orig.loadBush(g, vot, odm, c);
-				} catch (IOException e) {
-					orig.buildBush(g, vot, odm, c);
+				} catch (IOException ex) {
+					Bush bush = orig.buildBush(g, vot, odm, c);
+					
+					if (print) {
+						StringBuilder sb = new StringBuilder();
+						for (byte b : g.getMD5()) {
+							sb.append(String.format("%02X", b));
+						}
+
+						File file = new File(sb+"/"+o.getID()+"/"+c+"-"+vot+".bush");
+						file.getParentFile().mkdirs();
+						try {
+							PrintStream out = new PrintStream(file);
+
+							bush.toFile(out);
+							out.close();
+						} catch (FileNotFoundException e) {
+							e.printStackTrace();
+						}
+					}
 				}
 			}
 		}
