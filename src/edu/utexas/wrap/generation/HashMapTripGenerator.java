01b423374ea233c1d1dc2b2202ac3aea672a16cf
William Alexander <williamericalexander@gmail.com>
2019-06-24 13:39:17 -0500

    Missed a file in staging

--- src/edu/utexas/wrap/generation/HashMapTripGenerator.java
+++ src/edu/utexas/wrap/generation/HashMapTripGenerator.java
@@ -1,13 +1,38 @@
 package edu.utexas.wrap.generation;
 
+import java.util.Map;
+
 import edu.utexas.wrap.demand.MarketSegment;
-import edu.utexas.wrap.demand.PAMap;
 import edu.utexas.wrap.demand.containers.AggregatePAHashMap;
+import edu.utexas.wrap.net.Graph;
 
-public abstract class HashMapTripGenerator extends TripGenerator {
-
+public class HashMapTripGenerator extends TripGenerator {
+	
+	private Map<MarketSegment, Map<String, Float>> prodRates;
+	private Map<MarketSegment, Map<String, Float>> attrRates; 
+	private Graph graph;
+	
 	
 	@Override
-	public abstract AggregatePAHashMap generate(MarketSegment segment);
+	public AggregatePAHashMap generate(MarketSegment segment) {
+		Map<String,Float> segmentProdRates = prodRates.get(segment);
+		Map<String,Float> segmentAttrRates = attrRates.get(segment);
+		AggregatePAHashMap aggregate = new AggregatePAHashMap(graph);
+		
+		double prods = 0.0;
+		double attrs = 0.0;
+		
+		for (Zone z : graph.getZones()) {
+			for (String attribute : segmentProdRates.keySet()) {
+				prods += prodRates.get(attribute)*z.valueOf(attribute);
+			}
+			for (String attribute : segmentAttrRates.keySet()) {
+				attrs += attrRates.get(attribute)*z.valueOf(attribute);
+			}
+			aggregate.putProductions(z, prods);
+			aggregate.putAttractions(z, attrs);
+		}
+		return aggregate;
+	}
 
 }
