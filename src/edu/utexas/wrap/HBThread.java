a678705cc5d6947d8e827520bd60929b2eb7aeeb
KarthikVelayutham <karthik.velayutham@gmail.com>
2019-11-08 15:07:13 -0600

    Fixed some more bugs, moved splitHBW into the thread and changed its arguments

--- src/edu/utexas/wrap/HBThread.java
+++ src/edu/utexas/wrap/HBThread.java
@@ -5,6 +5,7 @@ import edu.utexas.wrap.demand.AggregatePAMatrix;
 import edu.utexas.wrap.demand.ModalPAMatrix;
 import edu.utexas.wrap.demand.ODMatrix;
 import edu.utexas.wrap.demand.PAMap;
+import edu.utexas.wrap.demand.containers.FixedMultiplierPassthroughPAMap;
 import edu.utexas.wrap.demand.containers.PerProductionZoneMultiplierPassthroughMatrix;
 import edu.utexas.wrap.distribution.FrictionFactorMap;
 import edu.utexas.wrap.distribution.GravityDistributor;
@@ -32,17 +33,21 @@ import java.util.stream.Stream;
 class HBThread extends Thread{
 	private Graph graph;
 	private Map<TimePeriod, Map<TripPurpose, Map<MarketSegment, Collection<ODMatrix>>>> hbODs;
+	private Map<MarketSegment, Double> splitRates;
 	private Map<TripPurpose,Map<MarketSegment,PAMap>> hbMaps;
 	private Map<MarketSegment, PAMap> pkMaps;
 	private Collection<MarketSegment> segments;
 
-	public HBThread(Graph graph, Map<MarketSegment, PAMap> pkMaps, Collection<MarketSegment> segments) {
+	public HBThread(Graph graph, Map<MarketSegment, Double> splitRates, Map<TripPurpose, Map<MarketSegment,PAMap>> hbMaps, Collection<MarketSegment> segments) {
 		this.graph = graph;
-		this.pkMaps = pkMaps;
+		this.splitRates = splitRates;
+		this.hbMaps = hbMaps;
 		this.segments = segments;
 	}
 
 	public void run() {
+		pkMaps = splitHBW(hbMaps, splitRates);
+
 		Map<MarketSegment, FrictionFactorMap> pkFFMaps = null; //TODO
 		HBPkGen hbPkGen = new HBPkGen(graph, pkFFMaps);
 		hbPkGen.start();
@@ -88,6 +93,27 @@ class HBThread extends Thread{
 	
 	public Map<TimePeriod, Map<TripPurpose, Map<MarketSegment, Collection<ODMatrix>>>> getODs(){
 		return hbODs;
+	}
+
+	private static Map<MarketSegment,PAMap> splitHBW(Map<TripPurpose,Map<MarketSegment,PAMap>> hbMaps, Map<MarketSegment, Double> splitRates) {
+		Map<MarketSegment,PAMap> hbwMaps = hbMaps.get(TripPurpose.HOME_WORK);
+
+		Map<MarketSegment,PAMap> pkMaps = new HashMap<MarketSegment,PAMap>();
+		Map<MarketSegment,PAMap> opMaps = new HashMap<MarketSegment,PAMap>();
+
+		hbwMaps.keySet().parallelStream().forEach(seg -> {
+			PAMap whole = hbwMaps.get(seg);
+			double pkShare = splitRates.get(seg);
+
+			PAMap peak = new FixedMultiplierPassthroughPAMap(whole, pkShare);
+			PAMap offpeak = new FixedMultiplierPassthroughPAMap(whole, 1-pkShare);
+
+			opMaps.put(seg, offpeak);
+			pkMaps.put(seg, peak);
+		});
+
+		hbMaps.put(TripPurpose.HOME_WORK, opMaps);
+		return pkMaps;
 	}
 
 	private static Map<MarketSegment, AggregatePAMatrix> peakDistribution(
@@ -231,7 +257,7 @@ class HBThread extends Thread{
 
 		public void run() {
 			try {
-				aggOPMtxs = offPeakDistribution(graph, hbMaps, opFFMaps);
+				aggOPMtxs = offPeakDistribution(g, hbMaps, opFFMaps);
 			} catch (IOException e) {
 				System.out.println("There is an IO Exception in the HB Thread's subthread, HBOpGen.");
 			}
