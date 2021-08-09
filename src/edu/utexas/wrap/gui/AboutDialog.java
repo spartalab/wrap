807bf0007c95e1bf8bed9db9c7d158c8e9c54cfc
americalexander <williamericalexander@gmail.com>
2021-08-09 15:41:48 -0500

    Added software version to About page, updated icon

--- src/edu/utexas/wrap/gui/AboutDialog.java
+++ src/edu/utexas/wrap/gui/AboutDialog.java
@@ -1,8 +1,12 @@
 package edu.utexas.wrap.gui;
 
+import java.lang.module.ModuleDescriptor.Version;
+import java.util.Optional;
+
 import javafx.application.HostServices;
 import javafx.fxml.FXML;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 
 public class AboutDialog {
 
@@ -15,6 +19,15 @@ public class AboutDialog {
 	private HostServices svcs;
 	
 	@FXML
+	private Label versionLabel;
+	
+	@FXML
+	private void initialize() {
+		Optional<Version> version = this.getClass().getModule().getDescriptor().version();
+		versionLabel.setText(version.isPresent()? version.get().toString() : "Unknown version");
+	}
+	
+	@FXML
 	private void openGPL() {
 		svcs.showDocument(gpl.getText());
 	}
@@ -25,7 +38,6 @@ public class AboutDialog {
 	}
 
 	public void setHostServices(HostServices hostServices) {
-		// TODO Auto-generated method stub
 		this.svcs = hostServices;
 	}
 }
